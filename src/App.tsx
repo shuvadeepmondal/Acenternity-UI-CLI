35124bf19467bcfc9c21279decd15fc767a29f51
shuvadeepmondal <121492365+shuvadeepmondal@users.noreply.github.com>
2024-11-13 23:41:25 +0530

    ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import CountBtn from '@/components/CountBtn';
-import ReactSVG from '@/assets/react.svg';
-import { Badge } from '@/components/ui/badge';
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+import Home from "./components/home/Home";
+import Navbar from "./components/utilis/Navbar";
+
+const App: React.FC = () => {
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col items-center gap-y-4">
-        <div className="inline-flex items-center gap-x-4">
-          <img src={ReactSVG} alt="React Logo" className="w-32" />
-          <span className="text-6xl">+</span>
-          <img src={'/vite.svg'} alt="Vite Logo" className="w-32" />
-        </div>
-        <a href="https://ui.shadcn.com" rel="noopener noreferrer nofollow" target="_blank">
-          <Badge variant="outline">shadcn/ui</Badge>
-        </a>
-        <CountBtn />
+    <>
+      <div>
+        <Navbar />
+        <Routes>
+        <Route path="/" element={<Home />} />
+
+        </Routes>
       </div>
-    </main>
+    </>
   );
 }
 
 export default App;
+
